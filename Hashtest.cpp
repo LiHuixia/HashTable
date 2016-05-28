a3dfaf9de79bd4adc145485358c1a530a61914ce
LiHuixia <1293970915@qq.com>
2016-05-28 20:12:47 +0800

    对哈希桶代码完善
    利用素数表，减少哈希冲突

---
+++ Hashtest.cpp
@@ -0,0 +1,17 @@
+//#include"HashTable.h"
+//#include"HASH.h"
+#include"TashTabltBucket.h"
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//First::testHashTable();
+	//Second::testFun();
+	//Test1();
+	//Test2();
+	Test();
+	system("pause");
+	return 0;
+}
